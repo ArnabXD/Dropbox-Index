6866a63cd307d92bf8ca72c7ef7d8a2dd15f0c45
Arnab Paryali <arnab.paryali@intglobal.com>
2025-04-14 11:32:59 +0530

    feat: update gitignore

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,4 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,10 +7,6 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export function loader({ context }: Route.LoaderArgs) {
-  return { message: context.cloudflare.env.VALUE_FROM_CLOUDFLARE };
-}
-
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return <h1>Hi</h1>;
 }
